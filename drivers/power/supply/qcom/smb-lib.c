098fb2c21293f330caa4004fc66cf4426d3fa510
engstk <eng.stk@sapo.pt>
2017-07-31 18:28:24 +0100

    drivers: misc: implement usb fast charge mode
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Enables force charging up to 900mA in usb mode
    
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -47,6 +47,10 @@
 #define	START_REPORT_BAT_TEMPRATURE	620
 /* Huaqin add for ZQL1650-76 Report battery temperature to upper by gaochao at 2018/02/07 end */
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
@@ -983,12 +987,12 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	u8 icl_options;
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
 
-	// AP: Fast charge for USB
-	if (icl_ua == USBIN_500MA)
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
 	{
 		icl_ua = USBIN_900MA;
-		pr_info("Boeffla-Kernel: Trigger USB fast charge with 900mA\n");
 	}
+#endif
 
 	/* power source is SDP */
 	switch (icl_ua) {
